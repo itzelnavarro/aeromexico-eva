43bfdcc08b9310e2e7b45e908d728ab9fcef7dd2
Itzel <itzo.navarro@gmail.com>
2021-10-14 13:21:16 -0500

    HpCard structure
    Maquetación de estructura para HpCard

--- src/components/HpCard.js
+++ src/components/HpCard.js
@@ -4,11 +4,27 @@ import HpCharacters from "../mock/hp-characters.json";
 const HpCard = () => {
 
     return (
-      <div>
-      {
+      <div className="hpCard">
+        <div className="imgCard">
+          <img />
+        </div>
+        <div className="infoCard">
+          <div className="statusCard">
+            <span> / </span>
+            <div className="favFlag"></div>
+          </div>
+          <h2></h2>
+          <div className="detailsCard">
+            <p><strong>Cumpleaños:</strong></p>
+            <p><strong>Género:</strong></p>
+            <p><strong>Color de ojos:</strong></p>
+            <p><strong>Color de pelo:</strong></p>
+          </div>
+        </div>
+      {/*
        HpCharacters.map (HpCharacter => <h1> {HpCharacter.name} </h1>)
-      }
-   </div>
+      */}
+      </div>
     );
   };
   
