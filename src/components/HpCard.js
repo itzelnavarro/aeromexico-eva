8a18e42352dd0b530a84c78b3acea8dabbb915f1
Itzel <itzo.navarro@gmail.com>
2021-10-14 22:15:38 -0500

    HpCard Background
    Fondo de color dependiendo la casa

--- src/components/HpCard.js
+++ src/components/HpCard.js
@@ -24,25 +24,41 @@ const HpCard = () => {
     }
   }, []);
 
+  function houseBg(house) {
+    switch(house){
+      case 'Hufflepuff':
+        return 'bgHuff';
+        break;
+      case 'Ravenclaw':
+        return 'bgRaven';
+        break;
+      case 'Slytherin':
+        return 'bgSlyth';
+        break;
+      default:
+          return 'bgGriff';
+    }
+  }
+
   const renderHpCharacter = () => {
-    console.log(hpData);
+
     return hpData.map((HpCharacter, index) => (
       <div className="hpCard" key={index}>
-        <div className="houseCard bgGriff">
+        <div className={"houseCard " + houseBg(HpCharacter.house)}>
           <div className="imgCard">
             <img src={HpCharacter.image}/>
-            </div>
+          </div>
         </div>
         <div className="infoCard">
           <div className="statusCard">
-            <span>Vivo / Estudiante</span>
+            <span>{HpCharacter.alive ? 'vivo' : 'muerto'}  {HpCharacter.hogwartsStudent ? '/ estudiante' : ''}{HpCharacter.hogwartsStaff ? '/ staff' : ''}</span>
             <div className="bookmarkCard"></div>
           </div>
           <h2>{HpCharacter.name}</h2>
           <div className="detailsCard">
-            <p><strong>Cumpleaños: </strong>{HpCharacter.dateOfBirth}</p>
+            <p><strong>Cumpleaños: </strong>{HpCharacter.dateOfBirth ? HpCharacter.dateOfBirth : 'unknown'}</p>
             <p><strong>Género: </strong>{HpCharacter.gender}</p>
-            <p><strong>Color de ojos: </strong>{HpCharacter.eyeColour}</p>
+            <p><strong>Color de ojos: </strong>{HpCharacter.eyeColour ? HpCharacter.eyeColour : 'unknown'}</p>
             <p><strong>Color de pelo: </strong>{HpCharacter.hairColour}</p>
           </div>
         </div>
